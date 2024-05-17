e420ebe3725f54923e80daac563ad33d2f1225f5
Asambler2 <chema508@hotmail.com>
2024-05-17 11:41:24 +0200

    Agregar archivos de proyecto.

---
+++ EjercicioFactoriaEjercitoC/Ejercito/EjercitoNuevo.cs
@@ -0,0 +1,61 @@
+﻿using EjercicioFactoriaEjercitoC.Unidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioFactoriaEjercitoC.Ejercito
+{
+    public class EjercitoNuevo : IEjercito, Ipresupuesto, IMilitarizable, ICosteable
+    {
+        public int NumElementos { get; set; }
+        public double CapacidadMilitar { get; set; } = 0;
+        public float Presupuesto { get; set; } = 0;
+        public List<IMilitarizable> EjercitoListaUnidades { get; set; } = new List<IMilitarizable>();
+        public string Titulo { get; set; }
+        public int Velocidad { get; set; } = 0;
+        public int Blindaje { get; set; } = 0;
+        public int PotenciaFuego { get; set; } = 0;
+        public float Precio { get; set; } = 0;    
+
+        public EjercitoNuevo(string NombreEjercito, float Presupuesto)
+        {
+            this.Titulo= NombreEjercito;
+            this.Presupuesto = Presupuesto; 
+        }
+
+        public void AddUnidad(IMilitarizable Unidad)
+        {
+            EjercitoListaUnidades.Add(Unidad);
+            this.NumElementos++;
+            this.PotenciaFuego += Unidad.PotenciaFuego;
+            this.Blindaje += Unidad.Blindaje;
+            this.Velocidad += Unidad.Velocidad;
+            this.Precio += (Unidad as ICosteable).Precio;
+            this.CapacidadMilitar = CalculoCapacidadMilitar();
+        }
+
+        public double CalculoCapacidadMilitar()
+        {
+            return (((double)this.PotenciaFuego * (double)this.Velocidad) / 2) / (100 - (double)this.Blindaje);
+        }
+
+        public string Mostrar()
+        {
+            return $"Nombre Ejercito: {this.Titulo}, Número de elementos: {this.NumElementos}, Potencia total: {this.PotenciaFuego}, " +
+                $"Blindaje total {this.Blindaje}, Velocidad total: {this.Velocidad}, Gasto total: {this.Precio}, Capacidad militar: {Math.Round(this.CapacidadMilitar, 2)}, " +
+                $"Presupuesto: {this.Presupuesto}, Presupuesto disponible: {this.Precio - this.Presupuesto}";
+        }
+        public string MostrarUnidadesEjercito()
+        {
+            string Cadena = "";
+            foreach (IMilitarizable Unidad in EjercitoListaUnidades)
+            {
+                Cadena += Unidad.Mostrar();
+                Console.WriteLine($"{Unidad.Mostrar()}");
+            }
+            return Cadena;
+        }
+    }
+}
---
+++ EjercicioFactoriaEjercitoC/Menu.cs
@@ -0,0 +1,148 @@
+﻿using EjercicioFactoriaEjercitoC.Ejercito;
+using EjercicioFactoriaEjercitoC.Unidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioFactoriaEjercitoC
+{
+    public class Menu
+    {
+        IMilitarizable GrupoEjercitos;
+
+        public void GenerarMenu()
+        {
+            GrupoEjercitos = GenerarGrupoEjercitos();
+            MenuEjercito();
+        }
+        public void MenuEjercito()
+        {
+            int Comando = 0;
+            Comando = OpcionesGrupoEjercitos();
+            switch(Comando)
+            {
+                case 1: ((EjercitoNuevo)GrupoEjercitos).AddUnidad(CrearEjercito());
+                    break;
+                case 2: ((EjercitoNuevo)GrupoEjercitos).MostrarUnidadesEjercito();
+                    break;  
+                case 3: IMilitarizable Ejercito = BuscarEjercito();
+                    MenuEjercito(Ejercito);
+                    break;
+            }
+            if (Comando != 0) MenuEjercito();
+        }
+
+        public EjercitoNuevo GenerarGrupoEjercitos()
+        {
+            string Nombre = "";
+            float Presupuesto;
+            Console.WriteLine("Introduce el nombre del grupo de ejercitos.");
+            Nombre = Console.ReadLine();
+            Console.WriteLine("Introduce el presupuesto.");
+            Presupuesto = float.Parse(Console.ReadLine());  
+            return new EjercitoNuevo(Nombre, Presupuesto);  
+        }
+
+        public int OpcionesGrupoEjercitos()
+        {
+            Console.WriteLine("Pulsa 0 para salir.");
+            Console.WriteLine("Pulsa 1 para generar un nuevo ejercito.");
+            if (((IEjercito)this.GrupoEjercitos).EjercitoListaUnidades.Count != 0)Console.WriteLine("Pulsa 2 para mostrar Ejercitos.");
+            if (((IEjercito)this.GrupoEjercitos).EjercitoListaUnidades.Count != 0) Console.WriteLine("Pulsa 3 para buscar un ejercito existente.");
+            return int.Parse(Console.ReadLine());
+        }
+
+        public IMilitarizable BuscarEjercito()
+        {
+            string ColeccionNombres = "";
+            Console.WriteLine("Introduce el nombre del ejercito a buscar");
+            string NombreEjercito = Console.ReadLine();
+            foreach(IMilitarizable Ejercito in ((IEjercito)this.GrupoEjercitos).EjercitoListaUnidades)
+            {
+                ColeccionNombres += $"\n{Ejercito.Titulo}";
+                if(Ejercito.Titulo.Equals(NombreEjercito))return Ejercito;
+            }
+            Console.WriteLine($"Introduce un nombre de ejercito existente de entre estos nombres. \n{ColeccionNombres}");
+            BuscarEjercito();
+            return new EjercitoNuevo("", 0);
+        }
+
+        public IMilitarizable CrearEjercito()
+        {
+            string NombreEjercito = "";
+            float Presupuesto = 0;
+            bool NombreYaExistente = false;
+            Console.WriteLine("Introduce el nombre del nuevo ejercito.");
+            NombreEjercito = Console.ReadLine();
+            foreach (IMilitarizable Ejercito in ((IEjercito)GrupoEjercitos).EjercitoListaUnidades)
+            {
+                if (Ejercito.Titulo== NombreEjercito) {
+                    Console.WriteLine("Ese Ejercito ya existe, introduce otro.");
+                    NombreYaExistente = true;
+                };
+            }
+            if (NombreYaExistente) CrearEjercito();
+            Console.WriteLine("Introduce el presupuesto del nuevo ejercito.");
+            Presupuesto = float.Parse(Console.ReadLine());
+            return new EjercitoNuevo(NombreEjercito, Presupuesto);
+        }
+        public int OpcionesEjercito(IMilitarizable Ejercito)
+        {
+            int Comando = 0;
+            Console.WriteLine("Introduce 1 para añadir una nueva unidad al ejercito.");
+            Console.WriteLine("Introduce 2 para mostar información del ejercito.");
+            if (((IEjercito)Ejercito).EjercitoListaUnidades.Count != 0) {
+                Console.WriteLine("Introduce 3 para mostrar todas las unidades del ejercito.");
+            }
+            Console.WriteLine("Introduce 0 ir al menu de ejercito.");
+            Comando = int.Parse(Console.ReadLine());
+            return Comando;
+        }
+
+        public void MenuEjercito(IMilitarizable Ejercito)
+        {
+            int Comando = 0;
+            Comando = OpcionesEjercito(Ejercito);
+            switch (Comando) {
+                case 1: Ejercito = CrearUnidad(Ejercito); 
+                    break;
+                case 2: Console.WriteLine(Ejercito.Mostrar());
+                    break;
+                case 3: ((IEjercito)Ejercito).MostrarUnidadesEjercito();
+                    break;
+            }
+            if (Comando != 0) MenuEjercito(Ejercito);
+        }
+
+        public IMilitarizable CrearUnidad(IMilitarizable Ejercito)
+        {
+            string NombreUnidad = "";
+            int Velocidad = 0;
+            int Blindaje = 0;
+            int PotenciaFuego = 0;
+            float ElPrecio = 0;
+            Console.WriteLine("Escribe el nombre de la unidad");
+            NombreUnidad = Console.ReadLine();
+            Console.WriteLine("Escribe la velocidad de la unidad");
+            Velocidad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escribe el blindaje de la unidad");
+            Blindaje = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escribe la potencia de fuego de la unidad");
+            PotenciaFuego = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escribe el precio de la unidad");
+            ElPrecio = float.Parse(Console.ReadLine());
+            if (((ICosteable)Ejercito).Precio + ElPrecio > (Ejercito as EjercitoNuevo).Presupuesto)
+            {
+                Console.WriteLine("Excede el presupuesto del ejercito, vuelve a generar una unidad con un precio menor o cero." 
+                    + " Excedente:" + ((Ejercito as EjercitoNuevo).Presupuesto - ElPrecio));
+                CrearUnidad(Ejercito);
+            }
+            IMilitarizable LaUnidadNueva = new Unidad(NombreUnidad, Velocidad, Blindaje, PotenciaFuego, ElPrecio);
+            ((IEjercito)Ejercito).AddUnidad(LaUnidadNueva);
+            return Ejercito;
+        }
+    }
+}
